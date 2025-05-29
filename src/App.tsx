b35292764dc3d375558a43b7200897feeedbfb1f
Chris <93202854+Queentaker@users.noreply.github.com>
2025-05-29 17:29:33 +0200

    feat(title): adding title to the section

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,17 @@ const routes = [{ path: "/", element: <Home /> }]
 
 function Home() {
 	return (
-		<section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      {About()}
-
-      {Experience()}
+		<section className="container grid items-center gap-4 pb-8 pt-6 md:py-10">
+			{About()}
+			<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+				Experience
+			</h1>
+			{Experience()}
+			<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+				Projects
+			</h1>
 			{Projects()}
-    </section>
+		</section>
 	)
 }
 
