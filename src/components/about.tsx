12060ff6ad99bb0fde4a781915fe7774adba4144
Chris <93202854+Queentaker@users.noreply.github.com>
2025-06-23 08:05:22 +0200

    feat(styiling): icon buttons added to about section. increased size compared to reguarl button

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,15 +1,62 @@
+import { Link } from "react-router-dom"
+import { siteConfig } from "@/config/site.ts"
+import { buttonVariants } from "@/components/ui/button.tsx"
+import { Icons } from "@/components/icons.tsx"
+
 export function About() {
 	return (
-		<div className="flex max-w-[980px]  items-start gap-2">
+		<div className="flex max-w-[980px]  items-start gap-2" id={"about"}>
 			<div>
-			<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-				Hi, I&apos;m Christopher Narayanan
-			</h1>
-			<p className="text-lg text-muted-foreground">
-				Hi, I&apos;m <span className="font-bold"> Christopher Narayanan </span>, a software
-				engineer with a passion for building innovative solutions. I love
-				exploring new technologies and creating impactful applications. </p>
+				<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+					Hi, I&apos;m Christopher
+				</h1>
+				<p className="text-lg text-muted-foreground">
+					I am a data driven connector. I work for SBB as data analyst intern,
+					worked at Novartis in their Digital Finance as and compeleted my CS
+					bachelor at UZH.{" "}
+				</p>
 
+				<div className="flex flex-1 items-center space-x-4">
+					<nav className="flex items-center space-x-1">
+						<Link to={siteConfig.links.cv} target="_blank" rel="noreferrer">
+							<div
+								className={buttonVariants({
+									size: "icon",
+									variant: "ghost",
+								})}
+							>
+								<Icons.cv className="size-8" />
+								<span className="sr-only">CV</span>
+							</div>
+						</Link>
+						<Link to={siteConfig.links.github} target="_blank" rel="noreferrer">
+							<div
+								className={buttonVariants({
+									size: "icon",
+									variant: "ghost",
+								})}
+							>
+								<Icons.gitHub className="size-8" />
+								<span className="sr-only">GitHub</span>
+							</div>
+						</Link>
+						<Link
+							to={siteConfig.links.linkedin}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<div
+								className={buttonVariants({
+									size: "icon",
+									variant: "ghost",
+								})}
+							>
+								<Icons.linkedin className="size-8" />
+								<span className="sr-only">GitHub</span>
+							</div>
+						</Link>
+					</nav>
+				</div>
 			</div>
 			<img
 				src={"src/assets/about/linkendinpic.jpg"}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none  [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -25,6 +25,13 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
+			svgSize: {
+				default: "[&_svg]:size-4",
+				sm: "[&_svg]:size-3",
+				icon: "[&_svg]:size-6",
+				lg: "[&_svg]:size-8",
+				xl: "[&_svg]:size-10",
+			}
     },
     defaultVariants: {
       variant: "default",
