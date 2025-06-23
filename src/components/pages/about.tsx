8641fb1495ec4b41d9c7dcafb2ff223cc8a2af1a
Chris <93202854+Queentaker@users.noreply.github.com>
2025-06-23 16:32:09 +0200

    feat(about page): make w-4/5 add boader to picture

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -5,63 +5,62 @@ import { Icons } from "@/components/icons.tsx"
 import { Card } from "@/components/ui/card.tsx"
 
 export function About() {
+	const Medialinks = <div className="flex flex-1 items-center space-x-4">
+		<Card className="flex items-center space-x-1">
+			<Link to={siteConfig.links.cv} target="_blank" rel="noreferrer">
+				<div
+					className={buttonVariants({
+						size: "icon",
+						variant: "ghost",
+					})}
+				>
+					<Icons.cv className="size-8" />
+					<span className="sr-only">CV</span>
+				</div>
+			</Link>
+			<Link to={siteConfig.links.github} target="_blank" rel="noreferrer">
+				<div
+					className={buttonVariants({
+						size: "icon",
+						variant: "ghost",
+					})}
+				>
+					<Icons.gitHub className="size-8" />
+					<span className="sr-only">GitHub</span>
+				</div>
+			</Link>
+			<Link
+				to={siteConfig.links.linkedin}
+				target="_blank"
+				rel="noreferrer"
+			>
+				<div
+					className={buttonVariants({
+						size: "icon",
+						variant: "ghost",
+					})}
+				>
+					<Icons.linkedin className="size-8" />
+					<span className="sr-only">GitHub</span>
+				</div>
+			</Link>
+		</Card>
+	</div>
 	return (
-		<div className="flex max-w-[980px]  items-start gap-2" id={"about"}>
+		<div className="flex w-4/5    items-start space-x-4 xl:space-x-6" id={"about"}>
 			<div>
 				<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
 					Hi, I&apos;m Christopher
 				</h1>
 				<p className="text-lg text-muted-foreground">
-					I am a data driven connector. I work for SBB as data analyst intern,
-					worked at Novartis in their Digital Finance as and compeleted my CS
-					bachelor at UZH.{" "}
+					Data
 				</p>
 
-				<div className="flex flex-1 items-center space-x-4">
-					<Card className="flex items-center space-x-1">
-						<Link to={siteConfig.links.cv} target="_blank" rel="noreferrer">
-							<div
-								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
-								})}
-							>
-								<Icons.cv className="size-8" />
-								<span className="sr-only">CV</span>
-							</div>
-						</Link>
-						<Link to={siteConfig.links.github} target="_blank" rel="noreferrer">
-							<div
-								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
-								})}
-							>
-								<Icons.gitHub className="size-8" />
-								<span className="sr-only">GitHub</span>
-							</div>
-						</Link>
-						<Link
-							to={siteConfig.links.linkedin}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div
-								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
-								})}
-							>
-								<Icons.linkedin className="size-8" />
-								<span className="sr-only">GitHub</span>
-							</div>
-						</Link>
-					</Card>
-				</div>
+				{Medialinks}
 			</div>
 			<img
 				src={"src/assets/about/linkendinpic.jpg"}
-				className="max-h-[300px] max-w-[300px] rounded-lg"
+				className="max-h-[300px] max-w-[300px] rounded-lg shadow-2xl"
 			/>
 		</div>
 	)
